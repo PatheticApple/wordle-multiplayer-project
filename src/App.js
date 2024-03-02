b5b0cd30cca6e1c37c3db7e2b9c6845c18534ab6
PatheticApple <garynguyenquy1301@gmail.com>
2024-03-02 16:50:11 +1000

    Setting up keyboard functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return; // Don't continue with the function
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard)
+    setCurrAttempt({ attempt: currAttempt.attempt, letterPos: currAttempt.letterPos + 1 })
+  }
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ attempt: currAttempt.attempt, letterPos: currAttempt.letterPos - 1 })
+  }
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 })
+  }
   return (
     <div className="App">
       <nav>
@@ -15,9 +37,11 @@ function App() {
           Wordle
         </h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
-        <Board />
-        <Keyboard />
+      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt, onDelete, onEnter, onSelectLetter }}>
+        <div className='game'>
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
 
     </div>
