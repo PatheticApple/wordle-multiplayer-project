34c0b6ad604073108abc9b9c79daa8b0397d139b
PatheticApple <garynguyenquy1301@gmail.com>
2024-03-02 17:21:29 +1000

    Guessing Words Functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import Board from './components/Board';
 import Keyboard from './components/Keyboard';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { boardDefault } from './components/Words';
+import { generateWordSet } from './components/Words';
 export const AppContext = createContext();
 
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
-
+  const [wordSet, setWordSet] = useState(new Set())
   const correctWord = "RIGHT";
+  const [disabledLetters, setDisabledLetters] = useState([])
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+    })
+  }, [])
 
   const onSelectLetter = (keyVal) => {
     if (currAttempt.letterPos > 4) return; // Don't continue with the function
@@ -30,7 +38,23 @@ function App() {
 
   const onEnter = () => {
     if (currAttempt.letterPos !== 5) return;
-    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 })
+
+
+    let currentWord = "";
+    for (let i = 0; i < 5; i++) {
+      currentWord += board[currAttempt.attempt][i]
+    }
+
+    if (wordSet.has(currentWord.toLowerCase())) {
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 })
+    }
+    else {
+      alert("Word Not Found")
+    }
+
+    if (currentWord === correctWord) {
+      alert("Game ended. You won!")
+    }
   }
   return (
     <div className="App">
@@ -39,7 +63,18 @@ function App() {
           Wordle
         </h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt, onDelete, onEnter, onSelectLetter, correctWord }}>
+      <AppContext.Provider value={{
+        board,
+        setBoard,
+        currAttempt,
+        setCurrAttempt, 
+        onDelete, 
+        onEnter, 
+        onSelectLetter, 
+        correctWord, 
+        disabledLetters,
+        setDisabledLetters
+      }}>
         <div className='game'>
           <Board />
           <Keyboard />
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,10 +1,10 @@
-import React , {useContext} from "react";
+import React , {useContext, useEffect} from "react";
 import { boardDefault } from "./Words";
 import App, { AppContext } from "../App";
 
 
 function Letter({ letterPos, attemptVal}) {
-    const {board, correctWord, currAttempt} = useContext(AppContext);
+    const {board, correctWord, currAttempt, disabledLetters, setDisabledLetters} = useContext(AppContext);
     const letter = board[attemptVal][letterPos];
 
     const correct = correctWord[letterPos] === letter
@@ -12,6 +12,15 @@ function Letter({ letterPos, attemptVal}) {
 
     const letterState = currAttempt.attempt > attemptVal &&
     (correct ? "correct" : almost ? "almost" : "error");
+    
+
+    useEffect(() => {
+        if (letter !== "" && !correct && !almost) {
+            setDisabledLetters((prev) => {
+                return [...prev, letter]
+            });
+        }
+    }, [currAttempt.attempt])
     return (
         <div className="letter" id={letterState}>{letter}</div>
     );
